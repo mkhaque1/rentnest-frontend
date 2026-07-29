dc2465308526c7bfe99cc90a65b47f2a2ac66b24
mkhaque1 <mkhaque29@gmail.com>
2026-07-29 12:38:25 +0200

    feat: enhance global styles and layout for improved UI and dark mode , fix tailwind for v4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en' className={inter.variable}>
+    <html lang='en' className={`dark ${inter.variable}`}>
       <body className='font-sans antialiased'>{children}</body>
     </html>
   );
